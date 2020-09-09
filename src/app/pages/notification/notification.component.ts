d415bafb9b1437fd53716fcbb30d3530606624a1
CaioHPM90 <caiomoloni99@gmail.com>
2020-09-08 22:49:09 -0300

    Tela de notificações terminada e tela home quase finalizada (parte dos objetos para receber o backend)

--- src/app/pages/notification/notification.component.ts
+++ src/app/pages/notification/notification.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationComponent implements OnInit {
 
+  //rgb(75, 75, 222) - cor das reações
   constructor() { }
 
   ngOnInit(): void {
