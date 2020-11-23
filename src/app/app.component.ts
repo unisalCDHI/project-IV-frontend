b4f8c5ec484a1f6f46c3c3577f1b4dc29f2ee6e9
davimargelo <davimargelo@hotmail.com>
2020-11-23 18:31:37 -0300

    search users input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { UserService } from './services/user.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from './shared/models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,9 @@ export class AppComponent {
   title = 'project-iv-frontend';
   token: string;
   id: string;
+  users: User[];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
     this.changeToken();
@@ -27,5 +30,11 @@ export class AppComponent {
   changeToken() {
     this.token = localStorage.getItem('token');
     this.id = localStorage.getItem('id');
+  }
+
+  searchUser(name) {
+    this.userService.findAllSearch(name).subscribe(res => {
+      this.users = res;
+    })
   }
 }
